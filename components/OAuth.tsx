5978d6e2d3ce856e850c36073d90107502d8501e
haya0002 <rahim.hayat@etudiant.univ-reims.fr>
2024-11-01 01:43:14 +0100

    v1.7: Stripe payment, dynamic data, OAuth authentication, and full main flow integration

--- components/OAuth.tsx
+++ components/OAuth.tsx
@@ -1,9 +1,23 @@
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, Alert } from "react-native";
 import CustomButton from "@/components/CustomButton";
 import { icons } from "@/constants";
+import { useOAuth } from "@clerk/clerk-expo";
+import { googleOAuth } from "@/lib/auth";
+import { router } from "expo-router";
 
 const OAuth = () => {
-  const handleGoogleSignIn = async () => {};
+  const { startOAuthFlow } = useOAuth({ strategy: "oauth_google" });
+
+  const handleGoogleSignIn = async () => {
+    const result = await googleOAuth(startOAuthFlow);
+
+    if (result.code === "session_exists" || result.code === "success") {
+      // Alert.alert("Success", "Session exists. Redirecting to home screen.");
+      router.replace("/(root)/(tabs)/home");
+    }
+
+    Alert.alert(result.success ? "Success" : "Error", result.message);
+  };
 
   return (
     <View>
